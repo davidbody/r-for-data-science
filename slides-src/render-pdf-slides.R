96fedc3b6185959f8ac07d0e35b89664ed6ec762
David W. Body <davidbody@bigcreek.com>
2019-05-15 16:18:46 -0500

    Only render slides that have changed

--- slides-src/render-pdf-slides.R
+++ slides-src/render-pdf-slides.R
@@ -1,6 +1,7 @@
 # Render Xaringan slides as PDF
 # https://github.com/yihui/xaringan/wiki/Export-Slides-to-PDF
 
+library(fs)
 library(pagedown)
 library(purrr)
 library(stringr)
@@ -10,8 +11,10 @@ slide_files <- list.files("./slides-src/", pattern = "\\d{2}-.*\\.Rmd")
 render_pdf <- function(file_name) {
   pdf_name <- str_glue("./slides/", str_replace(file_name, "Rmd", "pdf"))
   file_name <- str_glue("./slides-src/", file_name)
-  print(str_glue("Rendering {pdf_name}"))
-  chrome_print(file_name, pdf_name)
+  if (file_info(pdf_name)$modification_time < file_info(file_name)$modification_time) {
+    print(str_glue("Rendering {pdf_name}"))
+    chrome_print(file_name, pdf_name)
+  }
 }
 
-map(slide_files, render_pdf)
+walk(slide_files, render_pdf)
