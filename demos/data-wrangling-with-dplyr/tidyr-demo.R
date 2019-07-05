489140e4e0d3d9827e3d2e0cb75c52cf6c400bcc
David W. Body <davidbody@bigcreek.com>
2019-07-05 12:40:41 -0500

    Updated data wrangling demo

--- demos/data-wrangling-with-dplyr/tidyr-demo.R
+++ demos/data-wrangling-with-dplyr/tidyr-demo.R
@@ -21,16 +21,25 @@ new_name <- function(columns) {
 data_2016 <- read_excel(file_name, sheet = "2016", skip = 1) %>%
   rename_at(vars(contains("...")), new_name)
 
+# Gather labels
 step1 <- data_2016 %>%
   gather(-month, -day, key = "label", value = "value")
 
+# Separate labels into type and location
 step2 <- step1 %>%
   separate(label, into = c("type", "location"), sep = "_")
 
+# Spread values into separate columns based on type (price, quantity)
 step3 <- step2 %>%
   spread(key = "type", value = "value")
 
-step3 %>%
+sales_data_cleaned <- step3 %>%
   filter(!is.na(price) | !is.na(quantity)) %>%
   mutate(year = 2016, date = make_date(year, month, day)) %>%
   select(date, location, price, quantity)
+
+sales_data_cleaned %>%
+  ggplot(aes(quantity, price)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(vars(location))
